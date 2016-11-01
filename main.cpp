899eb0cb85f83ab0c3f65894eade3f70c5ad8936
Peng <peng.liu916@gmail.com>
2016-11-01 17:31:36 -0500

    Added MarkerMap detect

--- main.cpp
+++ main.cpp
@@ -7,34 +7,55 @@
 #include <aruco.h>
 #include <cvdrawingutils.h>
 
-using namespace cv;
+#define MARKER_MAP 0
+
+float cameraParams[] = {
+    1.0500229488335692e+03f, 0.f, 9.7136112531037020e+02f,
+    0.f, 1.0497918256490759e+03f, 5.4918051628416595e+02f,
+    0.f, 0.f, 1.f };
+float distorsionParams[] = {
+    2.9149065825195806e-02f, -2.9654300241936486e-02f,
+    -8.0282793629378685e-04f, -1.0993038057764051e-03f };
+
 int main()
 {
     IRGBDStreamer* pKinect2 =
         StreamFactory::createFromKinect2(true, true, true);
     pKinect2->StartStream();
     FrameData frames[IRGBDStreamer::kNumBufferTypes];
-    uint16_t ColorWidth, ColorHeight, InfraredWidth, InfraredHeight;
-    do
-    {
+    uint16_t ColorWidth, ColorHeight;
+    do {
         pKinect2->GetColorReso(ColorWidth, ColorHeight);
-    } while (ColorWidth == 0 || ColorHeight == 0);
+    } while (ColorHeight == 0);
 
-    aruco::MarkerDetector markerDetector;
-    cv::Mat infMat, infDisplayMat, colMat, colDetectMat, colDisplayMat;
-
-    float cameraParams[] = {
-        1.0500229488335692e+03f, 0.f, 9.7136112531037020e+02f,
-        0.f, 1.0497918256490759e+03f, 5.4918051628416595e+02f,
-        0.f, 0.f, 1.f};
-    float distorsionParams[] = {
-        2.9149065825195806e-02f, -2.9654300241936486e-02f,
-        -8.0282793629378685e-04f, -1.0993038057764051e-03f};
     cv::Mat cameraMatrix = cv::Mat(3, 3, CV_32F, cameraParams);
     cv::Mat distorsionMatrix = cv::Mat(4, 1, CV_32F, distorsionParams);
     aruco::CameraParameters
-        cp(cameraMatrix, distorsionMatrix, cv::Size(1920,1080));
-    //Code for Visualize live video streams
+        cp(cameraMatrix, distorsionMatrix, cv::Size(ColorWidth, ColorHeight));
+
+    cv::Mat colMat, colDetectMat, colDisplayMat, tmp;
+
+    // Read marker size if indicated
+    float markerSize = 0.37f;
+    // Read marker map
+    aruco::MarkerMap TheMarkerMapConfig;
+    TheMarkerMapConfig.readFromFile("config.yml");
+    aruco::MarkerDetector markerDetector;
+    markerDetector.setDictionary(TheMarkerMapConfig.getDictionary());
+    std::vector<aruco::Marker> vMarkers;
+
+#if MARKER_MAP
+    // Transform the markersetconfig to meter if is in pixels and the
+    // markersize indicated
+    if (TheMarkerMapConfig.isExpressedInPixels() && markerSize > 0)
+        TheMarkerMapConfig = TheMarkerMapConfig.convertToMeters(markerSize);
+    // Tracks the pose of the marker map
+    aruco::MarkerMapPoseTracker MSPoseTracker;
+    MSPoseTracker.setParams(cp, TheMarkerMapConfig);
+
+    std::vector<int> markers_from_set;
+#endif
+
     bool newData = false;
     while (true)
     {
@@ -45,24 +66,47 @@ int main()
                 frames[IRGBDStreamer::kInfrared]);
         } while (!newData);
 
-        if (frames[IRGBDStreamer::kColor].pData == nullptr) continue;
+        if (frames[IRGBDStreamer::kColor].pData == nullptr) {
+            continue;
+        }
         colMat = cv::Mat(ColorHeight, ColorWidth, CV_8UC4,
             frames[IRGBDStreamer::kColor].pData);
-        cvtColor(colMat, colMat, CV_BGRA2RGBA);
-        cvtColor(colMat, colDetectMat, CV_BGRA2GRAY);
 
-        float markerSize = 0.2f;
-        vector<aruco::Marker> vMarkers =
-            markerDetector.detect(colDetectMat, cp, markerSize);
-        for (unsigned int i = 0; i < vMarkers.size(); i++) {
-            cout << vMarkers[i] << endl;
-            vMarkers[i].draw(colDetectMat, Scalar(0, 0, 255), 2);
+        cv::cvtColor(colMat, tmp, CV_BGRA2RGBA);
+        // Kinect2 sensor already did a flip, so to keep pattern recognizable
+        // we need to flip it back;
+        cv::flip(tmp, colMat, 1);
+        cv::cvtColor(colMat, colDetectMat, CV_BGRA2GRAY);
+
+        vMarkers.clear();
+        vMarkers = markerDetector.detect(colDetectMat, cp, markerSize);
+
+#if MARKER_MAP
+        markers_from_set.clear();
+        markers_from_set = TheMarkerMapConfig.getIndices(vMarkers);
+        for (auto idx : markers_from_set) {
+            vMarkers[idx].draw(colMat, cv::Scalar(0, 0, 255), 2);
         }
-        for (unsigned int i = 0; i < vMarkers.size(); i++) {
-            aruco::CvDrawingUtils::draw3dCube(colMat, vMarkers[i], cp);
+
+        // Detect the 3d camera location wrt the markerset (if possible)
+        if (cp.isValid() && MSPoseTracker.estimatePose(vMarkers)) {
+            // If pose correctly computed, print the reference system
+            aruco::CvDrawingUtils::draw3dAxis(colMat, cp,
+                MSPoseTracker.getRvec(), MSPoseTracker.getTvec(),
+                TheMarkerMapConfig[0].getMarkerSize() * 2);
         }
-        imshow("Sensor0", colMat);
-        // waitKey is essential for imshow to work properly in loop
-        waitKey(10);
+#else
+        for (unsigned int i = 0; i < vMarkers.size(); i++) {
+                //cout << vMarkers[i] << endl;
+                vMarkers[i].draw(colMat, cv::Scalar(0, 0, 255), 1);
+            }
+        for (unsigned int i = 0; i < vMarkers.size(); i++) {
+            //aruco::CvDrawingUtils::draw3dCube(colMat, vMarkers[i], cp);
+            aruco::CvDrawingUtils::draw3dAxis(colMat, vMarkers[i], cp);
+        }
+#endif
+        cv::imshow("Sensor0", colMat);
+        // WaitKey is essential for imshow to work properly in loop
+        cv::waitKey(10);
     }
 }
