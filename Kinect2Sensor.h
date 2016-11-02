8c9da2e68d80709aba5fdf3c2d9c866b924b2be8
Peng <peng.liu916@gmail.com>
2016-11-01 19:49:49 -0500

    Squashed 'RGBDStreamer/' changes from 658c9dd..d1b8dd0
    d1b8dd0 [KinectVisualizer] Trimmed and reorganized project file to get rid of unused libs
    dec4900 [KinectVisualizer] Fixed memory leak bug,trimmed code to avoid unnecessary resolution check
    20f7d60 [KinectVisualizer] Fixed crash bug when destructing Kinect2Sensor: shouldn't open/close the WITABLE_HANDLE
    7336f60 [KinectVisualizer] Format to new code style and limit code row length
    
    git-subtree-dir: RGBDStreamer
    git-subtree-split: d1b8dd04a3003bb189c280986e238e76e72c5fe1

--- Kinect2Sensor.h
+++ Kinect2Sensor.h
@@ -9,58 +9,65 @@
 class RGBDSTREAMDLL_API Kinect2Sensor : public IRGBDStreamer
 {
 public:
-	Kinect2Sensor( bool EnableColor, bool EnableDepth, bool EnableInfrared );
-	~Kinect2Sensor();
-	virtual void GetColorReso( uint16_t& Width, uint16_t& Height ) const override;
-	virtual void GetDepthReso( uint16_t& Width, uint16_t& Height ) const override;
-	virtual void GetInfraredReso( uint16_t& Width, uint16_t& Height ) const override;
-	virtual void StartStream() override;
-	virtual void StopStream() override;
-	virtual bool GetNewFrames( FrameData& ColorFrame, FrameData& DepthFrame, FrameData& InfraredFrame ) override;
+    Kinect2Sensor(bool enableColor, bool enableDepth, bool enableInfrared);
+    ~Kinect2Sensor();
+    virtual void GetColorReso(uint16_t& width, uint16_t& height) const override;
+    virtual void GetDepthReso(uint16_t& width, uint16_t& height) const override;
+    virtual void GetInfraredReso(
+        uint16_t& width, uint16_t& height) const override;
+    virtual void StartStream() override;
+    virtual void StopStream() override;
+    virtual bool GetNewFrames(FrameData& colorFrame, FrameData& depthFrame,
+        FrameData& infraredFrame) override;
 
 protected:
-	// Current Kinect
-	IKinectSensor*				_pKinect2Sensor;
+    // Current Kinect
+    IKinectSensor* _pKinect2Sensor;
 
-	// Frame reader
-	IMultiSourceFrameReader*	_pMultiSourceFrameReader;
+    // Frame reader
+    IMultiSourceFrameReader* _pMultiSourceFrameReader;
 
-	// Frame data
-	FrameData					_pFrames[kNumBufferTypes][STREAM_BUFFER_COUNT];
+    // Frame data
+    FrameData _pFrames[kNumBufferTypes][STREAM_BUFFER_COUNT];
 
-	// Frame size
-	uint16_t					_ColorWidth = 0;
-	uint16_t					_ColorHeight = 0;
-	uint16_t					_DepthWidth = 0;
-	uint16_t					_DepthHeight = 0;
-	uint16_t					_InfraredWidth = 0;
-	uint16_t					_InfraredHeight = 0;
+    // Channel enabled
+    bool _depthEnabled = false;
+    bool _colorEnabled = false;
+    bool _infraredEnabled = false;
 
-	// Channel enabled
-	bool						_DepthEnabled = false;
-	bool						_ColorEnabled = false;
-	bool						_InfraredEnabled = false;
-
-	HRESULT Initialize();
-	void Shutdown();
+    HRESULT Initialize();
+    void Shutdown();
 
 private:
-	// Buffer control
-	uint8_t						_WritingIdx = 1;
-	std::atomic<uint8_t>		_LatestReadableIdx = 0;
-	std::atomic<uint8_t>		_ReadingIdx = 0;
+    class thread_guard
+    {
+    public:
+        thread_guard(std::thread& _t) :t(std::move(_t)) {}
+        thread_guard(std::thread&& _t) :t(std::move(_t)) {}
+        ~thread_guard() { if (t.joinable())t.join(); }
 
-	// Background thread control
-	std::atomic_bool			_Streaming;
-	std::thread					_BackGroundThread;
+        thread_guard(thread_guard const&) = delete;
+        thread_guard& operator=(thread_guard const&) = delete;
+    private:
+        std::thread t;
+    };
 
-	// Frame arrival event handle
-	WAITABLE_HANDLE				_hFrameArrivalEvent;
+    // Buffer control
+    uint8_t _writingIdx = 1;
+    std::atomic<uint8_t> _latestReadableIdx = 0;
+    std::atomic<uint8_t> _readingIdx = 0;
 
-	// Background thread procedure
-	void FrameAcquireLoop();
-	HRESULT ProcessingFrames( IMultiSourceFrameArrivedEventArgs* pArgs );
-	HRESULT ProcessDepthFrame( IDepthFrameReference* pDepthFrameRef );
-	HRESULT ProcessColorFrame( IColorFrameReference* pColorFrameRef );
-	HRESULT ProcessInfraredFrame( IInfraredFrameReference* pInfraredFrameRef );
+    // Background thread control
+    std::atomic_bool _streaming;
+    std::unique_ptr<thread_guard> _backGroundThread;
+
+    // Frame arrival event handle
+    WAITABLE_HANDLE _hFrameArrivalEvent;
+
+    // Background thread procedure
+    void FrameAcquireLoop();
+    HRESULT ProcessingFrames(IMultiSourceFrameArrivedEventArgs* pArgs);
+    HRESULT ProcessDepthFrame(IDepthFrameReference* pDepthFrameRef);
+    HRESULT ProcessColorFrame(IColorFrameReference* pColorFrameRef);
+    HRESULT ProcessInfraredFrame(IInfraredFrameReference* pInfraredFrameRef);
 };
